b6261b082e8d430d488daff3de4780eb83dcec40
Ninja <HBLegandary@live.nl>
2022-03-17 12:11:45 +0100

    Added Login redirection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
+
